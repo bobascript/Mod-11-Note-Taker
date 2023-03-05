e1f024153ceafb7afdf862433c058a6a73252e2e
mintghostii <alvarezjavi960@gmail.com>
2023-03-04 20:23:35 -0500

    got project working, will work on delete function if possible

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
 const express=require('express')
 const fs=require('fs')
 const path=require('path')
-const notes=require('./db/db.json')
+const {notes}=require('./db/db.json')
 
 const PORT = process.env.PORT || 3001
 
 const app=express()
+
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+function createNote(body,notesArr){
+    const note=body
+    notesArr.push(note)
+    fs.writeFileSync(
+        path.join(__dirname,'./db/db.json'),
+        JSON.stringify({notes:notesArr},null,2)
+    )
+    return body
+}
+
+//HTML Routes
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
+})
+
+
+
+//API Routes
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+app.post('/api/notes',(req,res)=>{
+    req.body.id=notes.length.toString()
+    const note=createNote(req.body,notes) 
+    res.json(note) 
+})
+
+app.listen(PORT,()=>{
+    console.log(`now listening on port 3001!`)
+})
